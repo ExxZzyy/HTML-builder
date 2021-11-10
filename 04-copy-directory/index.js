1996eb3025638b017272c101bcfc9c85342a40cc
ExxZzyy <66119764+ExxZzyy@users.noreply.github.com>
2021-11-10 22:13:00 +0300

    fix: remove files before copy in 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,6 +5,10 @@ const distDir = `${__dirname}/files-copy`;
 
 (async function () {
   try {
+    const filesDel = await fs.readdir(distDir);
+    filesDel.map((fileName) => {
+      fs.unlink(`${distDir}/${fileName}`);
+    });
     await fs.mkdir(distDir, {recursive:true});
     const files = await fs.readdir(srcDir);
     files.map((fileName) => {
