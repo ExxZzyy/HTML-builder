454d3ff849e895eb75aeddf55a35da8dabe1325e
ExxZzyy <66119764+ExxZzyy@users.noreply.github.com>
2021-11-10 23:31:23 +0300

    fix: remove error when folder not exist

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,11 +5,11 @@ const distDir = `${__dirname}/files-copy`;
 
 (async function () {
   try {
+    await fs.mkdir(distDir, {recursive:true});
     const filesDel = await fs.readdir(distDir);
     filesDel.map((fileName) => {
       fs.unlink(`${distDir}/${fileName}`);
     });
-    await fs.mkdir(distDir, {recursive:true});
     const files = await fs.readdir(srcDir);
     files.map((fileName) => {
       fs.copyFile(`${srcDir}/${fileName}`, `${distDir}/${fileName}`);
